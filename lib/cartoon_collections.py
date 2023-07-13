329cfdd61340dc2d42f697580df0a9e2a7187ae9
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-12 23:10:34 -0500

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,29 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+   return [print(f"{dwarves.index(name) + 1}. {name}") for name in dwarves]
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    new_list = []
+    [new_list.append(f"{item.capitalize()}!") for item in planeteer_calls]
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    new_list = []
+    [new_list.append(len(call)) for call in calls if len(call) > 4]
+    print(new_list)
+    if new_list != []:
+        return True
+    else:
+        return False
+    
+def find_the_cheese(food):
+    if "gouda" in food:
+        return "gouda"
+    elif "cheddar" in food:
+        return "cheddar"
+    elif "camembert" in food:
+        return "camembert"
+    else:
+        return None
+    
 
-def find_the_cheese():
-    pass
+print(find_the_cheese(['banana', 'cheddar', 'sock']))
